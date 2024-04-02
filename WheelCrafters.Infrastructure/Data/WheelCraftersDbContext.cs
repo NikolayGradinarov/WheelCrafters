40e16afcd0306b9738581be9b8329109124ea5c3
Nikolai Gradinarov <nikingr102@gmail.com>
2024-04-02 19:24:41 +0300

    Add project files.

---
+++ WheelCrafters.Infrastructure/Data/WheelCraftersDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WheelCrafters.Data
+{
+    public class WheelCraftersDbContext : IdentityDbContext
+    {
+        public WheelCraftersDbContext(DbContextOptions<WheelCraftersDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
